97a8126ae923bef34aaf678d0521d2c04919aa64
rongweijie <664510179@qq.com>
2020-03-11 18:04:18 +0800

    add eureka-client-user-service

---
+++ laisha/eureka-client-user-service/src/test/java/com/rrr/eurekaclientuserservice/EurekaClientUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.eurekaclientuserservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
