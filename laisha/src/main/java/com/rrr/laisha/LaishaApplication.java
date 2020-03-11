67e41a9c359dce5481dcd7177607c0812c7e3d3f
rongweijie <664510179@qq.com>
2020-03-11 15:11:27 +0800

    add laisha project

---
+++ laisha/src/main/java/com/rrr/laisha/LaishaApplication.java
@@ -0,0 +1,13 @@
+package com.rrr.laisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaishaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaishaApplication.class, args);
+    }
+
+}
---
+++ laisha/src/test/java/com/rrr/laisha/LaishaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.laisha;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaishaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
