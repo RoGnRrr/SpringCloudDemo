9524988fe942cbd1d699abb267fdeac38fe640fa
RoGnRrr <664510179@qq.com>
2020-03-13 01:33:34 +0800

    增加自定义元数据

--- laisha/eureka-client-article-service/src/main/java/com/rrr/eurekaclientarticleservice/ArticleController.java
+++ laisha/eureka-client-article-service/src/main/java/com/rrr/eurekaclientarticleservice/ArticleController.java
@@ -1,6 +1,8 @@
 package com.rrr.eurekaclientarticleservice;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +14,12 @@ public class ArticleController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/article/callHello")
     @ResponseBody
     public String callHello() {
@@ -22,6 +30,13 @@ public class ArticleController {
     @ResponseBody
     public String callHello2() {
         return restTemplate.getForObject("http://eureka-client-user-service/user/hello", String.class);
+    }
+
+    @GetMapping("/article/infos")
+    @ResponseBody
+    public Object infos() {
+        // return eurekaClient.getInstancesByVipAddress("eureka-client-user-service", false);
+        return discoveryClient.getInstances("eureka-client-user-service");
     }
 
 }
