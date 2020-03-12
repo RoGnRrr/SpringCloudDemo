963cad2a63d70d7d204e010aad46c598723ea746
RoGnRrr <664510179@qq.com>
2020-03-12 23:22:23 +0800

    add eureka-server-cluster

---
+++ laisha/eureka-server-cluster/src/test/java/com/rrr/eurekaservercluster/EurekaServerClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.eurekaservercluster;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
