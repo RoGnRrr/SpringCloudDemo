501abc0fb2690282378ae061e088df2b03500d62
RoGnRrr <664510179@qq.com>
2020-03-17 00:09:50 +0800

    add logger

--- laisha/eureka-feign/src/main/java/com/rrr/eurekafeign/api/client/UserRemoteClient.java
+++ laisha/eureka-feign/src/main/java/com/rrr/eurekafeign/api/client/UserRemoteClient.java
@@ -1,9 +1,10 @@
 package com.rrr.eurekafeign.api.client;
 
+import com.rrr.eurekafeign.FeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("eureka-client-user-service")
+@FeignClient(value = "eureka-client-user-service", configuration = FeignConfiguration.class)
 public interface UserRemoteClient {
 
     @GetMapping("/user/hello")
