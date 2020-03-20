a65de27ba924f490bd6e33390e3f6d679947dd57
RoGnRrr <664510179@qq.com>
2020-03-21 02:34:26 +0800

    增加 hystrix监控

--- laisha/hystrix-feign-demo/src/main/java/com/rrr/hystrixfeigndemo/HystrixFeignDemoApplication.java
+++ laisha/hystrix-feign-demo/src/main/java/com/rrr/hystrixfeigndemo/HystrixFeignDemoApplication.java
@@ -1,17 +1,29 @@
 package com.rrr.hystrixfeigndemo;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.rrr.hystrixfeigndemo.api.client")
 public class HystrixFeignDemoApplication {
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HystrixFeignDemoApplication.class, args);
