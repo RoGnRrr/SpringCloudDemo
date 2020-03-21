656dff56ac16ea37934b07aa9254d9899ea4f0c2
RoGnRrr <664510179@qq.com>
2020-03-21 22:37:32 +0800

    add hystrix-dashboard-demo

---
+++ laisha/hystrix-dashboard-demo/src/test/java/com/rrr/hystrixdashboarddemo/HystrixDashboardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrr.hystrixdashboarddemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
