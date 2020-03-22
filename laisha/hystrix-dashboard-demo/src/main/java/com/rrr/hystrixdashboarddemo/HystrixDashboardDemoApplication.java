3b84c9ecd6c01e8b79b20de94bd536886cf918cc
RoGnRrr <664510179@qq.com>
2020-03-22 23:40:26 +0800

    add turbine

--- laisha/hystrix-dashboard-demo/src/main/java/com/rrr/hystrixdashboarddemo/HystrixDashboardDemoApplication.java
+++ laisha/hystrix-dashboard-demo/src/main/java/com/rrr/hystrixdashboarddemo/HystrixDashboardDemoApplication.java
@@ -2,10 +2,14 @@ package com.rrr.hystrixdashboarddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableEurekaClient
+@EnableTurbine
 public class HystrixDashboardDemoApplication {
 
     public static void main(String[] args) {
